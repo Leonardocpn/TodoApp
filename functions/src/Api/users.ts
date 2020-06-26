eb7acde111d32d51a95f16f794f977f52d6fe7f8
Leonardocpn <leonardocpn@gmail.com>
2020-06-26 10:18:34 -0300

    feat: added create user and store in firestore

--- functions/src/Api/users.ts
+++ functions/src/Api/users.ts
@@ -1,6 +1,7 @@
-import firebase from "firebase";
 import { firebaseConfig } from "../utils/firebaseConfig";
+import firebase from "firebase";
 import { Request, Response } from "express";
+import { db } from "../utils/admin";
 
 firebase.initializeApp(firebaseConfig);
 
@@ -19,6 +20,55 @@ export const loginUser = async (request: Request, response: Response) => {
   } catch (err) {
     return response
       .status(403)
-      .json({ error: "Email or password invalid, please try again" });
+      .json({ error: "Email or password is invalid, please try again" });
+  }
+};
+
+export const createUser = async (request: Request, response: Response) => {
+  const newUser = {
+    firstName: request.body.firstName,
+    lastName: request.body.lastName,
+    email: request.body.email,
+    password: request.body.password,
+    username: request.body.username,
+  };
+  console.log(newUser);
+
+  try {
+    if (
+      (await db.collection("users").doc(`${newUser.username}`).get()).exists
+    ) {
+      return response
+        .status(400)
+        .json({ username: "this username is already taken" });
+    } else {
+      const userAuthenticated = await firebase
+        .auth()
+        .createUserWithEmailAndPassword(newUser.email, newUser.password);
+      const userId = userAuthenticated.user?.uid;
+      const token = await userAuthenticated.user?.getIdToken();
+
+      const userCredentials = {
+        firstName: newUser.firstName,
+        lastName: newUser.lastName,
+        username: newUser.username,
+        email: newUser.email,
+        createdAt: new Date().toISOString(),
+        userId,
+      };
+      await db
+        .collection("users")
+        .doc(`${newUser.username}`)
+        .set(userCredentials);
+      return response.status(201).json({ token });
+    }
+  } catch (error) {
+    if (error.code === "auth/email-already-in-use") {
+      return response.status(400).json({ email: "Email already in use" });
+    } else {
+      return response
+        .status(500)
+        .json({ general: "Something went wrong, please try again" });
+    }
   }
 };
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,12 @@
 import * as functions from "firebase-functions";
 import express from "express";
 import cors from "cors";
-import { loginUser } from "./Api/users";
+import { loginUser, createUser } from "./Api/users";
 
 const app = express();
 app.use(cors());
 
 app.post("/login", loginUser);
+app.post("/createUser", createUser);
 
 export const api = functions.https.onRequest(app);
